5fd44b751cd50f57f0b41a8dcdcb8465553fe07c
Gui Ruggiero <guilherme.ruggiero@gmail.com>
2024-07-12 22:35:02 +0000

    Firebase test not working, deferred

--- tests/functions/index.js
+++ tests/functions/index.js
@@ -1,6 +1,15 @@
 // import * as functions from "firebase-functions";
 const {onRequest} = require("firebase-functions/v2/https");
+
 const fs = require("fs");
+let promptRead = "";
+fs.readFile("prompt.txt", "utf8", (err, data) => {
+  if (err) {
+    console.error(err);
+  }
+
+  promptRead = data;
+});
 
 exports.helloWorld = onRequest({cors: true}, (request, response) => {
   let name = request.query.name;
@@ -13,13 +22,7 @@ exports.helloWorld = onRequest({cors: true}, (request, response) => {
 });
 
 exports.readFile = onRequest({cors: true}, (request, response) => {
-  fs.readFile("prompt.txt", "utf8", (err, data) => {
-    if (err) {
-      response.send("Error: " + err);
-    }
-
-    response.send("Data: " + data);
-  });
+  response.send("Data: " + promptRead);
 });
 
 exports.secret = onRequest({cors: true}, (request, response) => {
@@ -29,9 +32,27 @@ exports.secret = onRequest({cors: true}, (request, response) => {
 });
 
 // exports.firestore = onRequest({cors: true}, (request, response) => {
+//   const initializeApp = require("firebase/app");
+//   const {getFirestore, collection, addDoc} =
+//     require("firebase/firestore/lite");
 
+//   const firebaseConfig = {
+//     apiKey: process.env.FB_API_KEY,
+//     authDomain: process.env.FB_AUTH_DOMAIN,
+//     projectId: process.env.FB_PROJECT_ID,
+//     storageBucket: process.env.FB_STORAGE_BUCKET,
+//     messagingSenderId: process.env.FB_MESSAGING_SENDER_ID,
+//     appId: process.env.FB_APP_ID,
+//   };
+//   const app = initializeApp(firebaseConfig);
+//   const db = getFirestore(app);
+
+//   const mode = "dev";
+
+//   addDoc(collection(db, mode), "test"); // await?
+
+//   response.send("Logged!");
 // });
 
 // exports.gemini = onRequest({cors: true}, (request, response) => {
-
 // });
