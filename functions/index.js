9fbb541c3c0a3d26a44e95ed010a0d5ac1fd1e12
Gui Ruggiero <guilherme.ruggiero@gmail.com>
2024-07-16 17:28:08 +0000

    Function updates prompt daily from Google Docs

--- functions/index.js
+++ functions/index.js
@@ -5,7 +5,6 @@ const fs = require("fs");
 
 // Initializations
 const apiKey = process.env.GEMINI_API_KEY;
-// console.log(apiKey);
 const genAI = new GoogleGenerativeAI(apiKey);
 
 // Model setup
@@ -23,7 +22,6 @@ const generationConfig = {
   maxOutputTokens: 400,
   responseMimeType: "text/plain",
 };
-// console.log(generationConfig);
 
 const safetySettings = [
   {
@@ -43,7 +41,6 @@ const safetySettings = [
     threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
   },
 ];
-// console.log(safetySettings);
 
 const model = genAI.getGenerativeModel({
   model: modelChosen,
@@ -62,7 +59,6 @@ exports.guipt = onRequest({cors: true}, async (request, response) => {
   // console.log("chatHistory: " + chatHistory);
 
   // Initializes the chat
-  // const chat = model.startChat();
   const chat = model.startChat({history: chatHistory});
 
   // Gets user prompt from request
@@ -78,4 +74,31 @@ exports.guipt = onRequest({cors: true}, async (request, response) => {
 
   // Returns model response back to API caller
   response.send(guiptResponseText);
+});
+
+// --
+
+const {onSchedule} = require("firebase-functions/v2/scheduler");
+const jsdom = require("jsdom");
+const {htmlToText} = require("html-to-text");
+
+const url = process.env.PROMPT_URL;
+
+// Run once a day at midnight, for manual run: https://console.cloud.google.com/cloudscheduler
+exports.updatePrompt = onSchedule("every day 00:00", async (event) => {
+  const response = await fetch(url);
+  const document = await response.text();
+
+  const dom = new jsdom.JSDOM(document);
+  const contentHTML = dom.window.document.
+      querySelectorAll("div.doc-content")[0].innerHTML;
+
+  const contentText = htmlToText(contentHTML, {wordwrap: null});
+  const contentTextClean = contentText.replace(/\n{2,}/g, "\n");
+
+  fs.writeFileSync("prompt.txt", contentTextClean, "utf-8", (error) => {
+    if (error) {
+      console.error(error);
+    }
+  });
 });
