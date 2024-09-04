b03e91451e009135fb5b021681c92f8ba3c6e849
Gui Ruggiero <guilherme.ruggiero@gmail.com>
2024-09-04 23:15:06 +0000

    Who-Gui-ni's own function and prompt, index.js as aggregator

---
+++ functions/guipt.js
@@ -0,0 +1,78 @@
+const {GoogleGenerativeAI, HarmCategory, HarmBlockThreshold} =
+  require("@google/generative-ai");
+const fs = require("fs");
+const {onRequest} = require("firebase-functions/v2/https");
+
+// Initializations
+const apiKey = process.env.GEMINI_API_KEY;
+const genAI = new GoogleGenerativeAI(apiKey);
+
+// Gemini variation - "gemini-1.5-flash", "gemini-1.5-pro", "gemini-1.0-pro"
+const modelChosen = "gemini-1.5-flash";
+
+// Get prompt instructions from file
+const instructions = fs.readFileSync("prompt.txt", "utf8");
+// console.log(instructions);
+
+// Model configuration
+const generationConfig = {
+  temperature: 0.7, // default 1
+  topP: 0.95, // default 0.95
+  topK: 40, // default 40
+  maxOutputTokens: 400,
+  responseMimeType: "text/plain",
+};
+
+// Model safety settings
+const safetySettings = [
+  {
+    category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+    threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
+  },
+  {
+    category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+    threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
+  },
+  {
+    category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+    threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
+  },
+  {
+    category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+    threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+  },
+];
+
+// Model constructor
+const model = genAI.getGenerativeModel({
+  model: modelChosen,
+  systemInstruction: instructions,
+  generationConfig,
+  safetySettings,
+});
+
+exports.guipt = onRequest({cors: true}, async (request, response) => {
+  // Get chat history from request
+  let chatHistory = request.query.history;
+  if (!chatHistory) {
+    chatHistory = [];
+  }
+  // console.log("chatHistory: " + chatHistory);
+
+  // Initialize the chat
+  const chat = model.startChat({history: chatHistory});
+
+  // Get user prompt from request
+  let userInput = request.query.prompt;
+  if (!userInput) {
+    userInput = "Hi, what can you do?";
+  }
+
+  // Gemini API call
+  const result = await chat.sendMessage(userInput);
+  const guiptResponse = await result.response;
+  const guiptResponseText = guiptResponse.text();
+
+  // Returns model response back to API caller
+  response.send(guiptResponseText);
+});
