9b177731adc9d06fa6cedae85d636badf03dd96f
Gui Ruggiero <guilherme.ruggiero@gmail.com>
2024-11-02 23:23:20 +0100

    Updated updatePrompt

--- updatePrompt.js
+++ updatePrompt.js
@@ -5,18 +5,16 @@ import fs from "fs";
 
 const url = PROMPT_URL;
 
-// Gets source form page
+// Gets source from page
 const response = await fetch(url);
-const document = await response.text();
+const document = response.text();
 
 // Parses HTML to get only prompt content
 const dom = new jsdom.JSDOM(document);
 const contentHTML = dom.window.document.querySelectorAll("div.doc-content")[0].innerHTML;
 
 // Turns HTML into plain text
-const contentText = htmlToText(contentHTML, {
-    wordwrap: null,
-});
+const contentText = htmlToText(contentHTML, {wordwrap: null});
 const newPrompt = contentText.replace(/\n{2,}/g, "\n");
 
 const oldPrompt = fs.readFileSync("functions/prompt.txt", "utf8");
@@ -31,20 +29,3 @@ fs.writeFileSync("functions/prompt.txt", newPrompt, "utf-8", (error) => {
 });
 
 console.log("Prompt updated! Character diff: " + (newPrompt.length - oldPrompt.length));
-
-// --
-
-// // If new version is bigger (proxy for updated), save to file
-// if (newPrompt.length > oldPrompt.length) {
-//     fs.writeFileSync("functions/prompt.txt", newPrompt, "utf-8", (error) => {
-//         if (error) {
-//             console.log(error);
-//             return;
-//         }
-//     });
-
-//     console.log("Prompt updated: " + (newPrompt.length - oldPrompt.length) + " characters bigger.");
-// }
-// else {
-//     console.log("Prompt doesn't need an update.");
-// }
