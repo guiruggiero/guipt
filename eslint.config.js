a50d458d033b9a9b681e20b5f76191d6cc9a3693
Gui Ruggiero <guilherme.ruggiero@gmail.com>
2025-04-01 04:37:10 +0000

    Revamped ESLint and associated changes in codebase

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,50 @@
 import globals from "globals";
-import pluginJs from "@eslint/js";
+import js from "@eslint/js";
+import stylisticJs from "@stylistic/eslint-plugin-js";
+import eslintPluginYml from "eslint-plugin-yml";
 
 export default [
-  {languageOptions: { globals: globals.node }},
-  pluginJs.configs.recommended,
+  // Base JavaScript configuration
+  js.configs.recommended,
+
+  // Custom JavaScript configuration
+  {
+    files: ["*.js"],
+    plugins: {
+      js,
+      "@stylistic/js": stylisticJs,
+    },
+    languageOptions: {
+      // sourceType: "module",
+      globals: {
+        ...globals.node,
+      },
+    },
+    rules: {
+      "no-unused-vars": "error",
+      "@stylistic/js/indent": ["warn", 2],
+      "@stylistic/js/semi": "error",
+      "@stylistic/js/no-extra-semi": "warn",
+      "@stylistic/js/comma-spacing": ["warn", {"before": false, "after": true}],
+      "@stylistic/js/comma-dangle": ["warn", "always-multiline"],
+      "@stylistic/js/quotes": ["warn", "double"],
+      "@stylistic/js/spaced-comment": ["warn", "always"],
+      "@stylistic/js/arrow-spacing": "warn",
+      "@stylistic/js/block-spacing": ["warn", "never"],
+      "@stylistic/js/key-spacing": "warn",
+      "@stylistic/js/keyword-spacing": "warn",
+      "@stylistic/js/space-before-blocks": "warn",
+      "@stylistic/js/space-before-function-paren": ["warn", "never"],
+      "@stylistic/js/space-in-parens": "warn",
+    },
+  },
+
+  // YAML configuration
+  ...eslintPluginYml.configs["flat/standard"],
+  {
+    files: ["**/*.yml"],
+    rules: {
+      "yml/no-empty-mapping-value": "warn",
+    },
+  },
 ];
