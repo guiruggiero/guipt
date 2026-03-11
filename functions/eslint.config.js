25ef5b8324cfed44ec47bdd9ba3025bff9547de1
Gui Ruggiero <guilherme.ruggiero@gmail.com>
2026-03-11 04:41:13 +0000

    Migrated eslint config file to flat config format

---
+++ functions/eslint.config.js
@@ -0,0 +1,41 @@
+import {fileURLToPath} from "url";
+import path from "path";
+import {FlatCompat} from "@eslint/eslintrc";
+import {defineConfig} from "eslint/config";
+import js from "@eslint/js";
+import globals from "globals";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({baseDirectory: __dirname});
+
+export default defineConfig([
+  js.configs.recommended,
+  ...compat.extends("google"),
+  {
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: "module",
+      globals: {
+        ...globals.es2020,
+        ...globals.node,
+      },
+    },
+    rules: {
+      "valid-jsdoc": "off",
+      "require-jsdoc": "off",
+      "no-restricted-globals": ["error", "name", "length"],
+      "prefer-arrow-callback": "error",
+      "quotes": ["error", "double", {allowTemplateLiterals: true}],
+      "no-useless-assignment": "warn",
+    },
+  },
+  {
+    files: ["**/*.spec.*"],
+    languageOptions: {
+      globals: {
+        ...globals.mocha,
+      },
+    },
+  },
+]);
